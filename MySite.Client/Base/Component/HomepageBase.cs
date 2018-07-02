ec70ae7e525841e7de61de72d3afb23a6d6e74ed
Robert Szachnowski <rszachnowski@wi.ps.pl>
2018-07-02 18:07:03 +0200

    Rename and remove unnecessary statements.

---
+++ MySite.Client/Base/Component/HomepageBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.AspNetCore.Blazor.Components;
+using MySite.Client.Service;
+
+namespace MySite.Client.Base.Component
+{
+    public class HomepageBase : BlazorComponent
+    {
+
+    }
+}
