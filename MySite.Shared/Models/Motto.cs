203311daa4f78519b7786c7ee4ef7a90cee46735
Robert Szachnowski <rszachnowski@wi.ps.pl>
2018-07-02 18:07:23 +0200

    Create necessary model for homepage layout.

---
+++ MySite.Shared/Models/Motto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySite.Shared.Models
+{
+    public class Motto
+    {
+        public long MottoId { get; set; }
+
+        public string Sentence { get; set; }
+
+        public string Author { get; set; }
+
+        public bool Active { get; set; }
+    }
+}
