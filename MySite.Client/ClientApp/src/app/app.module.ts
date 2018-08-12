7e7bea22cda210887dfcd766835fb5e970fdc16f
Robert Szachnowski <rszachnowski@wi.ps.pl>
2018-08-12 11:17:13 +0200

    prepared routing

--- MySite.Client/ClientApp/src/app/app.module.ts
+++ MySite.Client/ClientApp/src/app/app.module.ts
@@ -1,16 +1,22 @@
+import { HomepageModule } from './homepage/homepage.module';
+import { HomeComponent } from './homepage/home/home.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: HomeComponent },
+  { path: '**', redirectTo: '/home' }
+];
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, HomepageModule, RouterModule.forRoot(routes)],
+  declarations: [AppComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
